4370091e947d82e99bb2fac54df6170031cbf6e2
Mihail Vojtuk <mihailvojtuk@gmail.com>
2024-12-08 12:37:35 +0900

    fix config.py

--- config/config.py
+++ config/config.py
@@ -1,3 +1,7 @@
-BOT_TOKEN = '7250321685:AAF-2XZiOL2BV7fWhNA5uNMTkE3EbWCaABY'
+import os
+from configparser import ConfigParser
+config = ConfigParser()
+config.read('config/config.ini')
+BOT_TOKEN = os.getenv('BOT_TOKEN', config.get(section='bot', option='token', fallback=None))
 if not BOT_TOKEN:
     exit('Please provide bot token environment variable')
